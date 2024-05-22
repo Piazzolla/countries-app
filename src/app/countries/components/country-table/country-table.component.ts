e5fbc35d05bdb44e42c2256cdca0b48614a94011
Mariano <macacozzz@hotmail.com>
2024-05-22 14:39:55 -0300

    showing country table

---
+++ src/app/countries/components/country-table/country-table.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'country-table',
+
+  templateUrl: './country-table.component.html',
+  styles: `
+    img {
+      width: 35px;
+      height: 35px;
+    }
+    .icon {
+      font-family: "Segoe UI Emoji", "Segoe UI Symbol", "Segoe UI", "Apple Color Emoji", "Twemoji Mozilla", "Noto Color Emoji", "Android Emoji";
+    }
+  `,
+  //changeDetection: ChangeDetectionStrategy.Default,
+})
+export class CountryTableComponent {
+  @Input()
+  public countries: Country[] = [];
+
+}
