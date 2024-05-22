45cf17ca188b8b42365c3450855c337f791ca0d1
Mariano <macacozzz@hotmail.com>
2024-05-22 17:16:13 -0300

    country information page

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({
@@ -29,6 +29,14 @@ export class CountriesService {
     return this.http.get<Country[]>(`${this.apiUrl}/region/${term}`)
     .pipe(
       catchError(() => of([]))
+    );
+  }
+
+  searchCountryByAlphaCode( code: string): Observable<Country | null> {
+    return this.http.get<Country[]>(`${this.apiUrl}/alpha/${code}`)
+    .pipe(
+      map( countries => countries.length > 0? countries[0]: null),
+      catchError(() => of(null))
     );
   }
 
